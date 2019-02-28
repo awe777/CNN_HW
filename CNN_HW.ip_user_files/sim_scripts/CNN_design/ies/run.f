7ae6a3f4c96b364279aa316a71960212dbde9ba1
Joshua Adiel Wijaya <awe777@uw.edu>
2019-02-28 10:47:45 +0700

    automated - block design generation

---
+++ CNN_HW.ip_user_files/sim_scripts/CNN_design/ies/run.f
@@ -0,0 +1,80 @@
+-makelib ies_lib/xilinx_vip -sv \
+  "D:/Xilinx/Vivado/2018.3/data/xilinx_vip/hdl/axi4stream_vip_axi4streampc.sv" \
+  "D:/Xilinx/Vivado/2018.3/data/xilinx_vip/hdl/axi_vip_axi4pc.sv" \
+  "D:/Xilinx/Vivado/2018.3/data/xilinx_vip/hdl/xil_common_vip_pkg.sv" \
+  "D:/Xilinx/Vivado/2018.3/data/xilinx_vip/hdl/axi4stream_vip_pkg.sv" \
+  "D:/Xilinx/Vivado/2018.3/data/xilinx_vip/hdl/axi_vip_pkg.sv" \
+  "D:/Xilinx/Vivado/2018.3/data/xilinx_vip/hdl/axi4stream_vip_if.sv" \
+  "D:/Xilinx/Vivado/2018.3/data/xilinx_vip/hdl/axi_vip_if.sv" \
+  "D:/Xilinx/Vivado/2018.3/data/xilinx_vip/hdl/clk_vip_if.sv" \
+  "D:/Xilinx/Vivado/2018.3/data/xilinx_vip/hdl/rst_vip_if.sv" \
+-endlib
+-makelib ies_lib/axi_infrastructure_v1_1_0 \
+  "../../../../CNN_HW.srcs/sources_1/bd/CNN_design/ipshared/ec67/hdl/axi_infrastructure_v1_1_vl_rfs.v" \
+-endlib
+-makelib ies_lib/axi_vip_v1_1_4 -sv \
+  "../../../../CNN_HW.srcs/sources_1/bd/CNN_design/ipshared/98af/hdl/axi_vip_v1_1_vl_rfs.sv" \
+-endlib
+-makelib ies_lib/processing_system7_vip_v1_0_6 -sv \
+  "../../../../CNN_HW.srcs/sources_1/bd/CNN_design/ipshared/70cf/hdl/processing_system7_vip_v1_0_vl_rfs.sv" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  "../../../bd/CNN_design/ip/CNN_design_processing_system7_0_0/sim/CNN_design_processing_system7_0_0.v" \
+  "../../../bd/CNN_design/ip/CNN_design_axi_lite_0_0/sim/CNN_design_axi_lite_0_0.v" \
+-endlib
+-makelib ies_lib/axi_lite_ipif_v3_0_4 \
+  "../../../../CNN_HW.srcs/sources_1/bd/CNN_design/ipshared/66ea/hdl/axi_lite_ipif_v3_0_vh_rfs.vhd" \
+-endlib
+-makelib ies_lib/lib_cdc_v1_0_2 \
+  "../../../../CNN_HW.srcs/sources_1/bd/CNN_design/ipshared/ef1e/hdl/lib_cdc_v1_0_rfs.vhd" \
+-endlib
+-makelib ies_lib/interrupt_control_v3_1_4 \
+  "../../../../CNN_HW.srcs/sources_1/bd/CNN_design/ipshared/a040/hdl/interrupt_control_v3_1_vh_rfs.vhd" \
+-endlib
+-makelib ies_lib/axi_gpio_v2_0_20 \
+  "../../../../CNN_HW.srcs/sources_1/bd/CNN_design/ipshared/a7c9/hdl/axi_gpio_v2_0_vh_rfs.vhd" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  "../../../bd/CNN_design/ip/CNN_design_axi_gpio_0_0/sim/CNN_design_axi_gpio_0_0.vhd" \
+-endlib
+-makelib ies_lib/generic_baseblocks_v2_1_0 \
+  "../../../../CNN_HW.srcs/sources_1/bd/CNN_design/ipshared/b752/hdl/generic_baseblocks_v2_1_vl_rfs.v" \
+-endlib
+-makelib ies_lib/axi_register_slice_v2_1_18 \
+  "../../../../CNN_HW.srcs/sources_1/bd/CNN_design/ipshared/cc23/hdl/axi_register_slice_v2_1_vl_rfs.v" \
+-endlib
+-makelib ies_lib/fifo_generator_v13_2_3 \
+  "../../../../CNN_HW.srcs/sources_1/bd/CNN_design/ipshared/64f4/simulation/fifo_generator_vlog_beh.v" \
+-endlib
+-makelib ies_lib/fifo_generator_v13_2_3 \
+  "../../../../CNN_HW.srcs/sources_1/bd/CNN_design/ipshared/64f4/hdl/fifo_generator_v13_2_rfs.vhd" \
+-endlib
+-makelib ies_lib/fifo_generator_v13_2_3 \
+  "../../../../CNN_HW.srcs/sources_1/bd/CNN_design/ipshared/64f4/hdl/fifo_generator_v13_2_rfs.v" \
+-endlib
+-makelib ies_lib/axi_data_fifo_v2_1_17 \
+  "../../../../CNN_HW.srcs/sources_1/bd/CNN_design/ipshared/c4fd/hdl/axi_data_fifo_v2_1_vl_rfs.v" \
+-endlib
+-makelib ies_lib/axi_crossbar_v2_1_19 \
+  "../../../../CNN_HW.srcs/sources_1/bd/CNN_design/ipshared/6c9d/hdl/axi_crossbar_v2_1_vl_rfs.v" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  "../../../bd/CNN_design/ip/CNN_design_xbar_0/sim/CNN_design_xbar_0.v" \
+-endlib
+-makelib ies_lib/proc_sys_reset_v5_0_13 \
+  "../../../../CNN_HW.srcs/sources_1/bd/CNN_design/ipshared/8842/hdl/proc_sys_reset_v5_0_vh_rfs.vhd" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  "../../../bd/CNN_design/ip/CNN_design_rst_ps7_0_50M_0/sim/CNN_design_rst_ps7_0_50M_0.vhd" \
+-endlib
+-makelib ies_lib/axi_protocol_converter_v2_1_18 \
+  "../../../../CNN_HW.srcs/sources_1/bd/CNN_design/ipshared/7a04/hdl/axi_protocol_converter_v2_1_vl_rfs.v" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  "../../../bd/CNN_design/ip/CNN_design_auto_pc_0/sim/CNN_design_auto_pc_0.v" \
+  "../../../bd/CNN_design/sim/CNN_design.v" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  glbl.v
+-endlib
+
