5f13211752baeb8914038405b0517da864f55e47
Joshua Adiel Wijaya <awe777@uw.edu>
2019-05-04 20:21:08 +0700

    automated - bitstream creation and export

--- CNN_HW.runs/impl_1/rundef.js
+++ CNN_HW.runs/impl_1/rundef.js
@@ -23,7 +23,7 @@ eval( EAInclude(ISEJScriptLib) );
 
 
 // pre-commands:
-ISETouchFile( "init_design", "begin" );
+ISETouchFile( "write_bitstream", "begin" );
 ISEStep( "vivado",
          "-log CNN_top_module_wrapper.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source CNN_top_module_wrapper.tcl -notrace" );
 
