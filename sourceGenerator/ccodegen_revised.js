a6b4de158d60e17aef1e4c11bbb12f1d03a1e3ca
Joshua Adiel Wijaya <awe777@uw.edu>
2019-05-10 14:43:14 +0700

    step 3, 4, 5 complete

--- sourceGenerator/ccodegen_revised.js
+++ sourceGenerator/ccodegen_revised.js
@@ -1,4 +1,14 @@
-function process(v = 0) {
+let debug = false;
+
+function debugToggle() {
+  debug = !debug;
+}
+
+function debugFlag(n = 0) {
+  debug ? console.log("Flag: end of step " + n + ", " + Date.now()) : null;
+}
+
+function float2Int(v = 0) {
   return (v < 128 && v >= -128) ? Math.floor(1024 * v) * 16384 : (v > 0) ? 0x7fffffff : 0xffffffff;
 }
 
@@ -25,30 +35,28 @@ function generate(v, r, im = [], im_w = 0, im_h = 0, wg = [], emulate = false) {
   bodyReset();
   const start = Date.now()
 
-  // step 1:  create square image array and square kernel array, if they aren't square already, and re-arrange them into 1-D array
-  //          (square kernel must be of length ${v - 1} * ${v - 1}, and square image must have a minimum of length ${v} * ${v})
+  // step 1:  create square kernel array, if it isn't square already, and re-arrange them into 1-D array
+  //          (square kernel must have a maximum of length ${v - 1} * ${v - 1})
   // step 2:  determine output size and length of array of arrays, based on *ORIGINAL* width and height of image and the size of the square kernel 
   //          (eg. 26 x 14 convolved with 5 x 5 should result in 22 x 10, not 22 x 22)
-  // step 3:  within C code, note where the input of square image and square kernel is transferred from JS to C
-  //          (in order to ease adaptation if other means of input method are used)
-  // step 4:  within C code, create algorithm that takes the square image array and break them into array of arrays with length ${v} * ${v}
-  //          (the length of array will be dependent on the size of the square kernel)
-  // step 5:  within C code, for each array member in the array of arrays, feed the HW input with the current member and the square kernel
-  // step 6:  within C code, plug the resulting output onto its position in the master output array, and not where the master output array is
-  //          (in order to ease adaptation if other means of output method are used)
-  // step 7:  within C code, generate method to output the code out of the C program
+  // step 3:  within C code, note where the master output array, input of image and square kernel is transferred from JS to C
+  //          (in order to ease adaptation if other means of I/O method are used)
+  // step 4:  within C code, create algorithm that takes the image array and break them into array of arrays with length ${v} * ${v}, or something as functional
+  //          (the length of array will be dependent on the size of the square kernel and hardware maximum capacity)
+  // step 5:  within C code, create algorithm that takes the square kernel array and re-mold it into a ${v - 1} * ${v - 1} square kernel, then feed it to HW input
+  // step 6:  within C code, for each array member in the array of arrays, feed the HW input with the current member 
+  // step 7:  within C code, plug the resulting output onto its position in the master output array
+  // step 8:  within C code, generate method to output the code out of the C program
 
   // PART: Step   1.0 : preliminary work
   const squareMapper = (dim, ar, w, h) => {
     return (c, i) => i < ar.length && i < w * h && i % dim < w ? ar[i % dim + w * Math.floor(i / dim)] : 0;
   }
-  // PART: Step   1.1 : square image array
-  const im_sq_dim = im_h > im_w ? (im_h > v ? im_h : v) : (im_w > v ? im_w : v); // max(im_h, im_w, v)
-  const im_sq = new Array(im_sq_dim * im_sq_dim).fill().map(squareMapper(im_sq_dim, im, im_w, im_h));
+  // PART: Step   1.1 : square kernel array
+  const wg_sq_dim = (v - 1) > Math.ceil(Math.sqrt(wg.length)) ? Math.ceil(Math.sqrt(wg.length)) : (v - 1);
+  const wg_sq = new Array(wg_sq_dim * wg_sq_dim).fill().map(squareMapper(wg_sq_dim, wg, wg_sq_dim, wg_sq_dim));
 
-  // PART: Step   1.2 : square kernel array
-  const wg_sq_dim = Math.ceil(Math.sqrt(wg.length));
-  const wg_sq = new Array((v - 1) * (v - 1)).fill().map(squareMapper(v - 1, wg, wg_sq_dim, wg_sq_dim));
+  debugFlag(1)
 
   // PART: Step   2   : output size determination
   const out_w = im_w - wg_sq_dim + 1;
@@ -56,15 +64,62 @@ function generate(v, r, im = [], im_w = 0, im_h = 0, wg = [], emulate = false) {
   const out_h = im_h - wg_sq_dim + 1;
   const out_h_rep = Math.ceil(1 + (out_h - v) / (v - wg_sq_dim + 1));
   // length of array of arrays will be out_w_rep * out_h_rep
+  // length of output array will be out_w * out_h
+
+  debugFlag(2)
+
+  // PART: Step   3   : transfer JS array to C
+  bodyPrint(`#include <stdio.h>`);
+  bodyPrint(`#include <stdint.h>`);
+  bodyPrint(`#include <sleep.h>`);
+  bodyPrint(`uint32_t *control = (uint32_t *) 0x40000000;`);
+  bodyPrint(`int main() {`)
+  bodyPrint(`  *(control) = 0;`)
+  bodyPrint(`  // STEP 3`)
+  bodyPrint(`  // importing adderMode toggle, image, and kernel data`)
+  // C variables that are initialized using JS variables (except ${v} or ${r}, a HW-specific JS variable) are the one needs to be replaced if I/O methods were to change
+  bodyPrint(`  int adderMode = ${true ? 1 : 0};`)
+  bodyPrint(`  int im_h = ${im_h}; // image height (vertical)`)
+  bodyPrint(`  int im_w = ${im_w}; // image width (horizontal)`)
+  bodyPrint(`  int wg_dim = ${wg_sq_dim} // kernel dimension, make sure this value is positive and less than ${v}`)
+  bodyPrint(`  int im[im_h * im_w] = {${im.map(float2Int).join()}}; // image array goes here`)
+  bodyPrint(`  int wg[wg_dim * wg_dim] = {${wg_sq.map(float2Int).join()}}; // square kernel array goes here`)
+  bodyPrint(`  // output of this (image, kernel) layer will be stored in out[]`)
+  bodyPrint(`  int out_h = im_h - wg_dim + 1;`)
+  bodyPrint(`  int out_w = im_w - wg_dim + 1;`)
+  bodyPrint(`  int out[out_w * out_h];`)
+
+  debugFlag(3)
+
+  // PART: Step   4   : find structuralized input jumping parameters to prepare image data before feeding to HW
+  bodyPrint(`  // STEP 4`)
+  bodyPrint(`  int jumps = ${v} - wg_dim + 1;`)
+  bodyPrint(`  int out_h_rep = out_h % jumps == 0 ? out_h / jumps : 1 + out_h / jumps; // Math.ceil(out_h / jumps)`)  
+  bodyPrint(`  int out_w_rep = out_w % jumps == 0 ? out_w / jumps : 1 + out_w / jumps; // Math.ceil(out_w / jumps)`)
+
+  debugFlag(4)
+
+  // PART: Step   5   : restructure and feed kernel data to HW
+  bodyPrint(`  // STEP 5`)
+  bodyPrint(`  int count0 = 0;`)
+  bodyPrint(`  for(count0 = 0; count0 < ${v - 1} * ${v - 1}; count0++) {`)
+  bodyPrint(`    if(count0 % ${v - 1} < wg_dim && count0 / ${v - 1} < wg_dim) {`)
+  bodyPrint(`      *(control + ${1 + v * v} + count0) = wg[wg_dim * (count0 / ${v - 1}) + count0 % ${v - 1}];`)
+  bodyPrint(`    } else {`)
+  bodyPrint(`      *(control + ${1 + v * v} + count0) = 0;`)
+  bodyPrint(`    }`)
+  bodyPrint(`  }`)
+
+  debugFlag(5)
   
   console.log("Hardware code generation time - " + (Date.now() - start) + " ms");
-  // step 8:  (optional) do software emulation of the hardware capabilities to provide time comparison between SW and HW execution time
+  // step 9:  (optional) do software emulation of the hardware capabilities to provide time comparison between SW and HW execution time
   if(emulate) {
-    // emulation starts here
+  // PART: Step   9   : software emulation with single-threaded JS
     const start_emu_0 = Date.now();
-    console.log("Software emulation (adder) time - " + (Date.now() - start_emu_0) + " ms")
+    console.log("Software emulation (adder) time - " + (Date.now() - start_emu_0) + " ms");
     
     const start_emu_1 = Date.now();
-    console.log("Software emulation (max-pooling) time - " + (Date.now() - start_emu_1) + " ms")
+    console.log("Software emulation (max-pooling) time - " + (Date.now() - start_emu_1) + " ms");
   }
 }
