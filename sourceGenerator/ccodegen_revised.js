5d782d5a315d335abafc529cb0d8d792ec53c7be
Joshua Adiel Wijaya <awe777@uw.edu>
2019-05-10 12:08:04 +0700

    step 1 and 2 complete, step 8 initial work

--- sourceGenerator/ccodegen_revised.js
+++ sourceGenerator/ccodegen_revised.js
@@ -16,7 +16,7 @@ function generate(v, r, im = [], im_w = 0, im_h = 0, wg = [], emulate = false) {
     wg = new Array(wg * wg).fill().map(() => 256 * Math.random() - 128)
   }
 
-  if(!(v && im_w * im_h === im.length)) { // sanity check
+  if(!(v && im_w * im_h === im.length && im_w >= 0)) { // sanity check
     return -1;
   }
    
@@ -26,7 +26,8 @@ function generate(v, r, im = [], im_w = 0, im_h = 0, wg = [], emulate = false) {
   const start = Date.now()
 
   // step 1:  create square image array and square kernel array, if they aren't square already, and re-arrange them into 1-D array
-  // step 2:  estimate output size, based on *ORIGINAL* width and height of image and the size of the square kernel 
+  //          (square kernel must be of length ${v - 1} * ${v - 1}, and square image must have a minimum of length ${v} * ${v})
+  // step 2:  determine output size and length of array of arrays, based on *ORIGINAL* width and height of image and the size of the square kernel 
   //          (eg. 26 x 14 convolved with 5 x 5 should result in 22 x 10, not 22 x 22)
   // step 3:  within C code, note where the input of square image and square kernel is transferred from JS to C
   //          (in order to ease adaptation if other means of input method are used)
@@ -36,10 +37,34 @@ function generate(v, r, im = [], im_w = 0, im_h = 0, wg = [], emulate = false) {
   // step 6:  within C code, plug the resulting output onto its position in the master output array, and not where the master output array is
   //          (in order to ease adaptation if other means of output method are used)
   // step 7:  within C code, generate method to output the code out of the C program
+
+  // PART: Step   1.0 : preliminary work
+  const squareMapper = (dim, ar, w, h) => {
+    return (c, i) => i < ar.length && i < w * h && i % dim < w ? ar[i % dim + w * Math.floor(i / dim)] : 0;
+  }
+  // PART: Step   1.1 : square image array
+  const im_sq_dim = im_h > im_w ? (im_h > v ? im_h : v) : (im_w > v ? im_w : v); // max(im_h, im_w, v)
+  const im_sq = new Array(im_sq_dim * im_sq_dim).fill().map(squareMapper(im_sq_dim, im, im_w, im_h));
+
+  // PART: Step   1.2 : square kernel array
+  const wg_sq_dim = Math.ceil(Math.sqrt(wg.length));
+  const wg_sq = new Array((v - 1) * (v - 1)).fill().map(squareMapper(v - 1, wg, wg_sq_dim, wg_sq_dim));
+
+  // PART: Step   2   : output size determination
+  const out_w = im_w - wg_sq_dim + 1;
+  const out_w_rep = Math.ceil(1 + (out_w - v) / (v - wg_sq_dim + 1));
+  const out_h = im_h - wg_sq_dim + 1;
+  const out_h_rep = Math.ceil(1 + (out_h - v) / (v - wg_sq_dim + 1));
+  // length of array of arrays will be out_w_rep * out_h_rep
   
-  console.log(Date.now() - start);
+  console.log("Hardware code generation time - " + (Date.now() - start) + " ms");
   // step 8:  (optional) do software emulation of the hardware capabilities to provide time comparison between SW and HW execution time
   if(emulate) {
     // emulation starts here
+    const start_emu_0 = Date.now();
+    console.log("Software emulation (adder) time - " + (Date.now() - start_emu_0) + " ms")
+    
+    const start_emu_1 = Date.now();
+    console.log("Software emulation (max-pooling) time - " + (Date.now() - start_emu_1) + " ms")
   }
 }
