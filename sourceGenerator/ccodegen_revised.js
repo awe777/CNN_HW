c39ab5cd7dfd7a479b77a5b3502fadb9b35a000e
Joshua Adiel Wijaya <awe777@uw.edu>
2019-05-10 16:35:33 +0700

    step 6, 7 complete

--- sourceGenerator/ccodegen_revised.js
+++ sourceGenerator/ccodegen_revised.js
@@ -44,27 +44,17 @@ function generate(v, r, im = [], im_w = 0, im_h = 0, wg = [], emulate = false) {
   // step 4:  within C code, create algorithm that takes the image array and break them into array of arrays with length ${v} * ${v}, or something as functional
   //          (the length of array will be dependent on the size of the square kernel and hardware maximum capacity)
   // step 5:  within C code, create algorithm that takes the square kernel array and re-mold it into a ${v - 1} * ${v - 1} square kernel, then feed it to HW input
-  // step 6:  within C code, for each array member in the array of arrays, feed the HW input with the current member 
-  // step 7:  within C code, plug the resulting output onto its position in the master output array
-  // step 8:  within C code, generate method to output the code out of the C program
+  // step 6:  within C code, for each array member in the array of arrays, feed the HW input with the current member and generate the output
+  // step 7:  within C code, generate method to output the code out of the C program
+  // step 8:  (optional) do software emulation of the hardware capabilities to provide time comparison between SW and HW execution time
 
-  // PART: Step   1.0 : preliminary work
-  const squareMapper = (dim, ar, w, h) => {
-    return (c, i) => i < ar.length && i < w * h && i % dim < w ? ar[i % dim + w * Math.floor(i / dim)] : 0;
-  }
-  // PART: Step   1.1 : square kernel array
+  // PART: Step   1   : square kernel array
   const wg_sq_dim = (v - 1) > Math.ceil(Math.sqrt(wg.length)) ? Math.ceil(Math.sqrt(wg.length)) : (v - 1);
-  const wg_sq = new Array(wg_sq_dim * wg_sq_dim).fill().map(squareMapper(wg_sq_dim, wg, wg_sq_dim, wg_sq_dim));
+  const wg_sq = new Array(wg_sq_dim * wg_sq_dim).fill().map((c, i) => i < wg.length ? wg[i % wg_sq_dim + wg_sq_dim * Math.floor(i / wg_sq_dim)] : 0);
 
   debugFlag(1)
 
-  // PART: Step   2   : output size determination
-  const out_w = im_w - wg_sq_dim + 1;
-  const out_w_rep = Math.ceil(1 + (out_w - v) / (v - wg_sq_dim + 1));
-  const out_h = im_h - wg_sq_dim + 1;
-  const out_h_rep = Math.ceil(1 + (out_h - v) / (v - wg_sq_dim + 1));
-  // length of array of arrays will be out_w_rep * out_h_rep
-  // length of output array will be out_w * out_h
+  // PART: Step   2   : output size determination - absorbed into step 3, length of out is the output size
 
   debugFlag(2)
 
@@ -75,19 +65,18 @@ function generate(v, r, im = [], im_w = 0, im_h = 0, wg = [], emulate = false) {
   bodyPrint(`uint32_t *control = (uint32_t *) 0x40000000;`);
   bodyPrint(`int main() {`)
   bodyPrint(`  *(control) = 0;`)
-  bodyPrint(`  // STEP 3`)
+  bodyPrint(`  // STEP 3 - commented variable initializations are to be replaced with a more suitable input data transfer`)
   bodyPrint(`  // importing adderMode toggle, image, and kernel data`)
   // C variables that are initialized using JS variables (except ${v} or ${r}, a HW-specific JS variable) are the one needs to be replaced if I/O methods were to change
-  bodyPrint(`  int adderMode = ${true ? 1 : 0};`)
-  bodyPrint(`  int im_h = ${im_h}; // image height (vertical)`)
-  bodyPrint(`  int im_w = ${im_w}; // image width (horizontal)`)
+  bodyPrint(`  int adderMode = ${true ? 1 : 0}; // 1 to run HW on adder mode, 0 to run HW on max-pooling mode`)
+  bodyPrint(`  int im_h = ${im_h}; // image height (vertical pixel length)`)
+  bodyPrint(`  int im_w = ${im_w}; // image width (horizontal pixel length)`)
   bodyPrint(`  int wg_dim = ${wg_sq_dim} // kernel dimension, make sure this value is positive and less than ${v}`)
-  bodyPrint(`  int im[im_h * im_w] = {${im.map(float2Int).join()}}; // image array goes here`)
-  bodyPrint(`  int wg[wg_dim * wg_dim] = {${wg_sq.map(float2Int).join()}}; // square kernel array goes here`)
-  bodyPrint(`  // output of this (image, kernel) layer will be stored in out[]`)
+  bodyPrint(`  int im[im_h * im_w] = {${im.map(float2Int).join()}}; // image array goes here, format is signed fixed point with radix at 8th (Q7.24), 14 LSBs will be ignored by HW`)
+  bodyPrint(`  int wg[wg_dim * wg_dim] = {${wg_sq.map(float2Int).join()}}; // square kernel array goes here, format is signed fixed point with radix at 8th (Q7.24), 14 LSBs will be ignored by HW`)
   bodyPrint(`  int out_h = im_h - wg_dim + 1;`)
   bodyPrint(`  int out_w = im_w - wg_dim + 1;`)
-  bodyPrint(`  int out[out_w * out_h];`)
+  bodyPrint(`  int out[out_w * out_h]; // output of this (image, kernel) layer will be stored here, format is signed fixed point with radix at 16th (Q15.16)`)
 
   debugFlag(3)
 
@@ -101,25 +90,73 @@ function generate(v, r, im = [], im_w = 0, im_h = 0, wg = [], emulate = false) {
 
   // PART: Step   5   : restructure and feed kernel data to HW
   bodyPrint(`  // STEP 5`)
-  bodyPrint(`  int count0 = 0;`)
-  bodyPrint(`  for(count0 = 0; count0 < ${v - 1} * ${v - 1}; count0++) {`)
-  bodyPrint(`    if(count0 % ${v - 1} < wg_dim && count0 / ${v - 1} < wg_dim) {`)
-  bodyPrint(`      *(control + ${1 + v * v} + count0) = wg[wg_dim * (count0 / ${v - 1}) + count0 % ${v - 1}];`)
+  bodyPrint(`  int count = 0;`)
+  bodyPrint(`  for(count = 0; count < ${v - 1} * ${v - 1}; count++) {`)
+  bodyPrint(`    if(count % ${v - 1} < wg_dim && count / ${v - 1} < wg_dim) {`)
+  bodyPrint(`      *(control + ${1 + v * v} + count) = wg[wg_dim * (count / ${v - 1}) + count % ${v - 1}];`)
   bodyPrint(`    } else {`)
-  bodyPrint(`      *(control + ${1 + v * v} + count0) = 0;`)
+  bodyPrint(`      *(control + ${1 + v * v} + count) = 0;`)
   bodyPrint(`    }`)
   bodyPrint(`  }`)
 
   debugFlag(5)
+
+  // PART: Step   6   : feed image data to HW, run HW, and obtain output
+  bodyPrint(`  // STEP 6`)
+  bodyPrint(`  int jumpRight = 0;`)
+  bodyPrint(`  int jumpDown = 0;`)
+  bodyPrint(`  int moveRight = 0;`)
+  bodyPrint(`  int moveDown = 0;`)
+  bodyPrint(`  for(jumpDown = 0; jumpDown < out_h_rep; jumpDown++) {`)
+  bodyPrint(`    for(jumpRight = 0; jumpRight < out_w_rep; jumpRight++) {`)
+  bodyPrint(`      for(moveDown = 0; moveDown < ${v}; moveDown++) {`)
+  bodyPrint(`        for(moveRight = 0; moveRight < ${v}; moveRight++) {`)
+  bodyPrint(`          if(jumps * jumpRight + moveRight < im_w && jumps * jumpDown + moveDown < im_h) {`)
+  bodyPrint(`            *(control + 1 + ${v} * moveDown + moveRight) = im[im_w * (jumps * jumpDown + moveDown) + (jumps * jumpRight + moveRight)];`)
+  bodyPrint(`          } else {`)
+  bodyPrint(`            *(control + 1 + ${v} * moveDown + moveRight) = 0;`)
+  bodyPrint(`          }`)
+  bodyPrint(`        }`)
+  bodyPrint(`      }`)
+  bodyPrint(`      if(adderMode = 1) {`)
+  bodyPrint(`        *(control) = 0xffffffff;// turn on all the bits to enable adder mode`)
+  bodyPrint(`      } else {`)
+  bodyPrint(`        *(control) = 0x007fffff; // turn off the first 9 bits to enable max-pooling mode`)
+  bodyPrint(`      }`)
+  bodyPrint(`      usleep(${Math.ceil(16.5 * (v * (v - wg_sq_dim) + (v - wg_sq_dim + 1) + Math.ceil(Math.ceil(Math.log2(v * v)) / r) + 1) / 1000)}); // sleep length is mostly determined by HW specifications and performance (added 20% extra time as buffer)`)
+  bodyPrint(`      *(control) = 0; `)
+  bodyPrint(`      for(moveDown = 0; moveDown < jumps; moveDown++) {`)
+  bodyPrint(`        for(moveRight = 0; moveRight < jumps; moveRight++) {`)
+  bodyPrint(`          if(jumps * jumpRight + moveRight < out_w && jumps * jumpDown + moveDown < out_h) {`)
+  bodyPrint(`            out[out_w * (jumps * jumpDown + moveDown) + (jumps * jumpRight + moveRight)] = (int)*(control + ${2 * (v * (v - 1) + 1)} + ${v} * moveDown + moveRight);`)
+  bodyPrint(`          }`)
+  bodyPrint(`        }`)
+  bodyPrint(`      }`)
+  bodyPrint(`    }`)
+  bodyPrint(`  }`)
+
+  debugFlag(6)
+
+  // PART: Step   7   : provide output data to suitable output channels, or, (from here) repurpose the output as the input of another run
+  bodyPrint(`  // STEP 7 - at this point, output of the (image, kernel) layer is in out[]`)
+  bodyPrint(`  printf("result = [\\n");`)
+  bodyPrint(`  for(count = 0; count < out_w * out_h; count++) {`)
+  bodyPrint(`    printf("%f,\\n", (double) (out[count] / 65536);`)
+  bodyPrint(`  }`)
+  bodyPrint(`  printf("]\\n");`)
+  bodyPrint(`}`)
+
+  debugFlag(7)
   
   console.log("Hardware code generation time - " + (Date.now() - start) + " ms");
-  // step 9:  (optional) do software emulation of the hardware capabilities to provide time comparison between SW and HW execution time
   if(emulate) {
-  // PART: Step   9   : software emulation with single-threaded JS
+  // PART: Step   8   : software emulation with single-threaded JS
     const start_emu_0 = Date.now();
     console.log("Software emulation (adder) time - " + (Date.now() - start_emu_0) + " ms");
     
     const start_emu_1 = Date.now();
     console.log("Software emulation (max-pooling) time - " + (Date.now() - start_emu_1) + " ms");
   }
+
+  debugFlag(8)
 }
